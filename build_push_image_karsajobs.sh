7c6c5465fcbfd86816439113ec6d7f0fbfe57b90
louisaldorio <louisaldorio@gmail.com>
2023-02-04 13:58:38 +0700

    adding sh file

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# membuat image baru dari Dockerfile yang berada di direktori ini
+docker build -t karsajobs:latest .
+
+# menampilkan daftar image yang ada
+docker images
+
+# membuat tag untuk image yang baru dibuat , tag ini akan digunakan untuk push image ke GitHub Container Registry
+docker tag karsajobs:latest ghcr.io/louisaldorio/karsajobs:latest
+
+# melakukan echo untuk memasukkan CR_PAT (GitHub Personal Access Token) ke dalam docker login
+echo $CR_PAT | docker login ghcr.io -u louisaldorio --password-stdin
+
+# melakukan push image ke GitHub Container Registry
+docker push ghcr.io/louisaldorio/karsajobs:latest
